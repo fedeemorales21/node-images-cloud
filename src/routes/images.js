3f970004171b3bb14639c442bb80887b58df4022
Federico Morales <fedeemorales21@gmail.com>
2020-10-18 20:43:25 -0300

    route

--- src/routes/images.js
+++ src/routes/images.js
@@ -1,12 +1,41 @@
 const { Router } = require('express')
 const router = Router()
 
-router.get('/', ( req,res ) => {
-    res.render('index')
+const cloudinary = require('cloudinary').v2
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
 })
 
-router.post('/add', ( req,res ) => {
-    
+const Photo = require('../models/Photo')
+
+router.get('/', async(req,res) => {
+    try {
+        const photos = await Photo.find()
+        res.render('index' , { photos })
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+
+router.post('/add', async( req,res ) => {
+    try {
+        console.log(req.body)
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+
+router.post('/dalete/:id', async( req,res ) => {
+    try {
+        const id = req.params.id
+        
+    } catch (error) {
+        console.log(error)
+    }
 })
 
 
